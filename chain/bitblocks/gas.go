f398450dfbf6a4c6be46d0654986d6fdf19cfe33
ReniR256 <75802042+ReniR256@users.noreply.github.com>
2021-04-22 00:04:26 +0100

    Create gas.go

---
+++ chain/bitblocks/gas.go
@@ -0,0 +1,9 @@
+package bitblocks
+
+import "github.com/ReniR256/multichain/chain/bitcoin"
+
+// GasEstimator re-exports bitcoin.GasEstimator.
+type GasEstimator = bitcoin.GasEstimator
+
+// NewGasEstimator re-exports bitcoin.NewGasEstimator.
+var NewGasEstimator = bitcoin.NewGasEstimator
